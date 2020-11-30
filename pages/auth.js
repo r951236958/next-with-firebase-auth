6f556a016c7e8499a3fc1ecd15135302469e927a
r951236958 <r951236958@gmail.com>
2020-12-01 00:30:14 +0800

    更新檔案結構

--- pages/auth.js
+++ pages/auth.js
@@ -1,4 +1,4 @@
-import FirebaseAuth from '../components/FirebaseAuth'
+import FirebaseAuth from '../src/components/FirebaseAuth'
 
 const Auth = () => {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import useSWR from 'swr'
 import Link from 'next/link'
-import { useUser } from '../utils/auth/useUser'
+import { useUser } from '../src/utils/auth/useUser'
 
 const fetcher = (url, token) =>
   fetch(url, {
